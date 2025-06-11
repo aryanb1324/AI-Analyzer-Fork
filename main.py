5da06c52e5fdecb7c6f07a5a45afafee3a50214b
MKcool99 <127639243+MKcool99@users.noreply.github.com>
2025-06-11 13:41:52 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import os
+import pandas as pd
+
+#pip install openpyxl xlrd
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    file_data = None
+    return render_template('index.html', file_data=file_data)
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        end = file.filename.split('.')[-1]
+        engine = 'openpyxl' if end == 'xlsx' else 'xlrd'
+        df = pd.read_excel(file, engine=engine)
+        file_data = df.to_html(classes='table table-bordered', index=False)
+    return render_template('index.html', file_data=file_data)   
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=5000)
+
+# To run the application, use the command: python main.py
